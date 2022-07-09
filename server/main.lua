352fc3ded7bb0d9f72f21ad0001eeed585fc7eca
RadhwaneDZ <71345996+RadhwaneDZ@users.noreply.github.com>
2022-07-09 18:45:39 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,19 +1,8 @@
-ESX = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+local QBCore = exports['qb-core']:GetCoreObject()
 
-ESX.RegisterUsableItem('kitavanzato', function(source)
-    local xPlayer = ESX.GetPlayerFromId(source)
-	if xPlayer.job.name == "mechanic" then 
-	    TriggerClientEvent('nk-repair:client:triggerMenu', source)
-    end
-end)
-
-RegisterServerEvent("nk-repair:removeitem")
-AddEventHandler("nk-repair:removeitem", function(item, quantita)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    if xPlayer.getInventoryItem(item).count >= (quantita) then
-        xPlayer.removeInventoryItem(item, quantita)
-    else
-        TriggerClientEvent('esx:showNotification', xPlayer.source, 'You don\'t have enough: '..item.label)
+QBCore.Functions.CreateUseableItem("repairitem", function(source, item)
+    local Player = QBCore.Functions.GetPlayer(source)
+	if Player.Functions.RemoveItem(item.name, 1, item.slot) then
+        TriggerClientEvent("qb-repair:client:RepairVeh", source, item.name)
     end
 end)
